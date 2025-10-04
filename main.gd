47bbcc7298b7f071ee5b9545a8f9b43bb131c815
Dmitriy.Obraztsov <Dmitriy.Obraztsov@b1.by>
2025-10-04 22:07:02 +0400

    fix multiplayer

--- main.gd
+++ main.gd
@@ -44,3 +44,23 @@ func start_game():
 	# Hide the UI and unpause to start the game.
 	$UI.hide()
 	get_tree().paused = false
+	if multiplayer.is_server():
+		change_level.call_deferred(load("res://Scenes/Level1.tscn"))
+	
+# Call this function deferred and only on the main authority (server).
+func change_level(scene: PackedScene):
+	# Remove old level if any.
+	var level = $Level
+	for c in level.get_children():
+		level.remove_child(c)
+		c.queue_free()
+	# Add new level.
+	level.add_child(scene.instantiate())
+
+
+# The server can restart the level by pressing Home.
+func _input(event):
+	if not multiplayer.is_server():
+		return
+	if event.is_action("ui_home") and Input.is_action_just_pressed("ui_home"):
+		change_level.call_deferred(load("res://Scenes/Level1.tscn"))
